71ad1adebcd13dfa005b35ae96fe8ddd0e0a9a54
김민정 <dst04021@gmail.com>
2023-09-14 17:40:37 +0900

    feat: dlclass, newclass 영역 레이아웃 완성
    dlclass, newclass json 파일 추가
    이미지 추가
    swiper 효과 추가

---
+++ js/slideclass.js
@@ -0,0 +1,149 @@
+window.onload = function(){
+// 콤마 기능
+function priceToString(price) {
+    return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+// data.json을 로딩. 연결시켜준다.
+const xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function(event){
+    const req = event.target;
+    if(req.readyState === XMLHttpRequest.DONE){
+        const str = req.response;
+        // 글자로 온 데이터를 객체로 변환
+        // 글자가 json 규칙대로 만들어진 문자열.
+        // 그러므로 json 글자를 객체로 변환해서 활용한다.
+        let obj = JSON.parse(str);
+
+        DL_CLASS = obj.dlclass;
+        NEW_CLASS = obj.newclass;
+        showDlClass(); // 마감임박 클래스 화면에 배치
+        showNewClass(); // 신규 클래스 화면에 배치
+    }
+}
+// 자료를 호출한다.
+xhttp.open("GET", "data.json")
+// 웹브라우저 기능 실행 할 수 있도록 요청.
+xhttp.send();
+// 할인 물품
+let DL_CLASS;
+let NEW_CLASS;
+let dlClassTag = document.getElementById("data-dlclass");
+let newClassTag = document.getElementById("data-newclass");
+
+
+// 마감임박 클래스 화면 출력 기능
+function showDlClass(){
+    let html = `
+    <div class="swiper sw-dlclass">
+    <div class="swiper-wrapper">
+    `;
+    
+    DL_CLASS.forEach(function(item){
+        let tag = `
+        <div class="swiper-slide class-slide" style="background-image: url('image/${item.pic}')">
+            <a href="${item.link}" class="class-box">
+                <span class="class-type">${item.tag}</span>
+                <div class="class-txt">
+                    <!-- 제품정보 -->
+                    <p class="class-info">${item.name}</p>
+                    <!-- 제품가격 -->
+                    <p class="class-price">${priceToString(item.price)}원</p>
+                </div>
+            </a>
+        </div>
+        `;
+        html += tag;
+    });
+    html += `
+    </div>
+    </div>
+    `;
+    // 마감임박 클래스 Swiper
+    dlClassTag.innerHTML = html;
+    var swiper = new Swiper(".sw-dlclass", {
+        slidesPerView: 'auto',
+        speed: 3000,
+        spaceBetween: 30,
+        freeMode: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        autoplay: {     //자동슬라이드 (false-비활성화)
+            delay: 0, // 시간 설정
+            disableOnInteraction: false, // false-스와이프 후 자동 재생
+        },
+        loop : true,   // 슬라이드 반복 여부
+        loopAdditionalSlides : 1,
+        centeredSlides: true
+    });
+    // 슬라이드 호버시 일시정지
+    let $slides = document.querySelectorAll('.dlclass .class-slide');
+        for (let i of $slides) {
+        i.addEventListener('mouseover', function(){
+            swiper.autoplay.stop();
+        });
+        i.addEventListener('mouseout', function(){
+            swiper.autoplay.start();
+    });
+    }
+}
+
+// 신규 클래스 화면 출력 기능
+function showNewClass(){
+    let html = `
+    <div class="swiper sw-newclass">
+    <div class="swiper-wrapper">
+    `;
+    
+    NEW_CLASS.forEach(function(item){
+        let tag = `
+        <div class="swiper-slide class-slide" style="background-image: url('image/${item.pic}')">
+            <a href="${item.link}" class="class-box">
+                <span class="class-type">${item.tag}</span>
+                <div class="class-txt">
+                    <!-- 제품정보 -->
+                    <p class="class-info">${item.name}</p>
+                    <!-- 제품가격 -->
+                    <p class="class-price">${priceToString(item.price)}원</p>
+                </div>
+            </a>
+        </div>
+        `;
+        html += tag;
+    });
+    html += `
+    </div>
+    </div>
+    `;
+    // 신규 클래스 Swiper
+    newClassTag.innerHTML = html;
+    var swiper = new Swiper(".sw-newclass", {
+        slidesPerView: 'auto',
+        speed: 3000,
+        spaceBetween: 30,
+        freeMode: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        autoplay: {     //자동슬라이드 (false-비활성화)
+            delay: 0, // 시간 설정
+            disableOnInteraction: false, // false-스와이프 후 자동 재생
+        },
+        loop : true,   // 슬라이드 반복 여부
+        loopAdditionalSlides : 1,
+        centeredSlides: true
+    });
+    // 슬라이드 호버시 일시정지
+    let $slides = document.querySelectorAll('.newclass .class-slide');
+        for (let i of $slides) {
+        i.addEventListener('mouseover', function(){
+            swiper.autoplay.stop();
+        });
+        i.addEventListener('mouseout', function(){
+            swiper.autoplay.start();
+    });
+    }
+}
+}
